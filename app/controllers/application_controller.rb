efcdf0ec6ea33f8e2e34443ea1ccee425ce0ccbd
Kaku <kaku.takanori.1026@gmail.com>
2019-06-24 16:40:06 +0000

    曜日の色変更完了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,5 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper #sessionコントローラーでヘルパーが使用できる。
   include AttendancesHelper #attendancesコントローラーでヘルパーが使用できる。
+  $days_of_the_week = %w{日 月 火 水 木 金 土}
 end
