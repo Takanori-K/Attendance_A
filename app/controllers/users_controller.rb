00a704c3de85d9ffe7d6f595871aecb6e62bb615
Kaku <kaku.takanori.1026@gmail.com>
2019-07-16 16:02:32 +0000

    affiliation変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -80,7 +80,7 @@ class UsersController < ApplicationController
   private
     
     def user_params
-      params.require(:user).permit(:name, :email, :department, :password, :password_confirmation)
+      params.require(:user).permit(:name, :email, :affiliation, :password, :password_confirmation)
     end
     
     def basic_info_params
