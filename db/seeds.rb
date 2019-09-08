74a45e538d32d7db3347a13aeda39feec3bfe11e
Kaku <kaku.takanori.1026@gmail.com>
2019-09-08 06:24:28 +0000

    show_page_項目追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,14 +3,28 @@ User.create!(name:  "管理者",
              password:              "password",
              password_confirmation: "password",
              employee_number:        "1",
+             uid:                    "111",
              admin: true)
-             
-5.times do |n|
-  name  = Faker::Name.name
-  email = "email#{n+1}@sample.com"
-  password = "password"
-  User.create!(name:  name,
-               email: email,
-               password:              password,
-               password_confirmation: password)
-end
+
+User.create!(name: "上長A",
+             email: "email1@gmail.com",
+             password:              "password",
+             password_confirmation: "password",
+             employee_number:       "2",
+             uid:                   "222",
+             superior: true)
+
+User.create!(name: "上長B",
+             email: "email2@gmail.com",
+             password:              "password",
+             password_confirmation: "password",
+             employee_number:       "3",
+             uid:                   "333",
+             superior: true)
+
+User.create!(name: "一般",
+             email: "email3@gmail.com",
+             password:              "password",
+             password_confirmation: "password",
+             employee_number:       "4",
+             uid:                   "444")             
